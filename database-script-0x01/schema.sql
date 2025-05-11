85e7c2ec6769c39eabd5915fed23da6cc0d54ae5
Kidiavayi Atala <kidiavayi@buupass.com>
2025-05-11 16:08:59 +0300

    feat: add initial database schema for users, properties, bookings, payments, reviews, and messages

---
+++ database-script-0x01/schema.sql
@@ -0,0 +1,73 @@
+-- USER
+CREATE TABLE IF NOT EXISTS users (
+    user_id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15),
+    role ENUM('guest', 'host', 'admin') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- PROPERTY
+CREATE TABLE IF NOT EXISTS property (
+    property_id CHAR(36) PRIMARY KEY,
+    host_id CHAR(36) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    pricepernight DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (host_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- BOOKING
+CREATE TABLE IF NOT EXISTS booking (
+    booking_id CHAR(36) PRIMARY KEY,
+    user_id CHAR(36) NOT NULL,
+    property_id CHAR(36) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status ENUM('pending', 'confirmed', 'cancelled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (property_id) REFERENCES property(property_id) ON DELETE CASCADE
+);
+
+-- PAYMENT
+CREATE TABLE IF NOT EXISTS payment (
+    payment_id CHAR(36) PRIMARY KEY,
+    booking_id CHAR(36) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM('credit_card', 'paypal', 'stripe') NOT NULL,
+    FOREIGN KEY (booking_id) REFERENCES booking(booking_id) ON DELETE CASCADE
+);
+
+-- REVIEW
+CREATE TABLE IF NOT EXISTS review (
+    review_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    rating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES property(property_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- MESSAGES
+CREATE TABLE IF NOT EXISTS messages (
+    message_id CHAR(36) PRIMARY KEY,
+    sender_id CHAR(36) NOT NULL,
+    recipient_id CHAR(36) NOT NULL,
+    property_id CHAR(36),
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (recipient_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (property_id) REFERENCES property(property_id) ON DELETE CASCADE
+);
