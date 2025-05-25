95f159b529d7c9bef109c7e0091a4f51aa4a1c6b
Kidiavayi Atala <kidiavayi@buupass.com>
2025-05-25 15:09:47 +0300

    feat: add missing semicolon and format SQL join queries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,6 +6,11 @@ INNER JOIN users ON booking.user_id = users.user_id;
 
 SELECT * FROM
 property
-LEFT JOIN review ON property.id = review.property_id;
+LEFT JOIN review ON property.id = review.property_id\;
+
+
+SELECT * FROM
+user
+FULL OUTER JOIN booking ON user.user_id = booking.user_id
 
 
